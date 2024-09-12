d415fc1eacbf947a6bb4aa8ba709ab57fa03e54e
KOMAL4214 <jksai2006@gmail.com>
2024-09-12 15:30:43 +0530

    Add files via upload

---
+++ 51.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("enter the number");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+		 	if(i==j)
+		 	{
+		 		printf("1");
+			 }
+			 else if(i>j||i<j)
+			 {
+			 	printf("0");
+			 }
+		}
+		printf("\n");
+	 } 
+}
