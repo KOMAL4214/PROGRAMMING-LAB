2ee84f5be164712189debc4ebb3c814e66882a27
KOMAL4214 <jksai2006@gmail.com>
2024-09-17 16:07:58 +0530

    Update 50.cpp

--- 50.cpp
+++ 50.cpp
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	char i,j,ch;
-	for(i='A';i<='ch';i++)
+	int i,j,n;
+	for(i=1;i<=n;i++)
 	{
-		for(j='A';j<='ch';j++)
+		for(j=1;j<=i;j++)
 		{
-			printf("%c",i);
+			printf("%c",(char)(i+64));
 		}
 		printf("\n");
 	 } 
