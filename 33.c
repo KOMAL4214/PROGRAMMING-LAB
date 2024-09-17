b031a1e1f08bdcde5c510bf0e9788867446e86ad
KOMAL4214 <jksai2006@gmail.com>
2024-09-17 15:39:00 +0530

    Update 33.c

--- 33.c
+++ 33.c
@@ -2,10 +2,12 @@
 #include <math.h>
 int main()
 {
-           int i,max=0,min=0,n,k,o;
+           int i,max,min,n,k,o;
           printf("program to print max and secmax");
           printf("\nenter the numbers");
           scanf("%d",&n);
+           scanf("%d",&o);
+           max=min=o;
           for(i=1;i<=n;i++)
           {
           scanf("%d",&o);
