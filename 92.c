cab1d2262fe6f1480320d9d2da71604275819d59
KOMAL4214 <jksai2006@gmail.com>
2024-11-17 20:49:47 +0530

    Add files via upload

---
+++ 92.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+printf("enter the size of array");
+scanf("%d",&n);
+int  *ptr,a[n];
+printf("enter the list of elements");
+for(i=0;i<n;i++)
+{
+ scanf("%d",&a[i]);
+ }
+ ptr=&a[0];
+ for(i=0;i<n;i++)
+{
+ if(*ptr<=a[i])
+ {
+ ptr=&a[i];
+ }
+ }
+ printf("\nmax element is %d",*ptr);
+return 0;
+}
